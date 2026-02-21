8997a0a74d0e981fc3de582606418c353b80b386
QiMirda <admiquel94@gmail.com>
2026-02-21 18:36:27 +0100

    rest(api): Add API functions to CRUD sites

--- lib/api-sites.ts
+++ lib/api-sites.ts
@@ -22,4 +22,69 @@ export const siteApis = {
       throw error;
     }
   },
+  createSite: async function ({ body }: { body: Site }): Promise<Site> {
+    try {
+      const response = await fetch(`${API_URL}/api/sites`, {
+        method: "POST",
+        body: JSON.stringify(body),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error("Failed to fetch sites:", error);
+      throw error;
+    }
+  },
+  updateSite: async function (
+    siteId: Site["id"],
+    patch: Partial<Site>,
+  ): Promise<Site> {
+    try {
+      const response = await fetch(`${API_URL}/api/sites/${siteId}`, {
+        method: "PATCH",
+        body: JSON.stringify(patch),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error("Failed to fetch sites:", error);
+      throw error;
+    }
+  },
+  deleteSite: async function (siteId: Site["id"]): Promise<void> {
+    try {
+      const response = await fetch(`${API_URL}/api/sites/${siteId}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error("Failed to fetch sites:", error);
+      throw error;
+    }
+  },
 };
