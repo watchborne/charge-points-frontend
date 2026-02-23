4c2a06fb29896ef31f20b0c9c90f186eae8acf3f
QiMirda <admiquel94@gmail.com>
2026-02-23 23:07:46 +0100

    chore(components): Add basic components

---
+++ components/ui/datepicker.tsx
@@ -0,0 +1,50 @@
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from "@radix-ui/react-popover";
+import { format } from "date-fns";
+import classNames from "classnames";
+import { fr } from "date-fns/locale";
+import { CalendarIcon, Calendar } from "lucide-react";
+
+import { Button } from "./button";
+
+export const Datepicker = ({
+  value,
+  onChange,
+  placeholder = "Choisir une date",
+  disabled,
+}: {
+  value?: Date;
+  onChange: (date?: Date) => void;
+  placeholder?: string;
+  disabled?: (date: Date) => boolean;
+}) => {
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          className={classNames(
+            "w-full justify-start text-left font-normal",
+            !value && "text-muted-foreground",
+          )}
+        >
+          <CalendarIcon className="mr-2 h-4 w-4 shrink-0" />
+          {value ? format(value, "dd MMMM yyyy", { locale: fr }) : placeholder}
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto p-0" align="start">
+        <Calendar
+          mode="single"
+          selected={value}
+          onSelect={onChange}
+          disabled={disabled}
+          locale={fr}
+          initialFocus
+        />
+      </PopoverContent>
+    </Popover>
+  );
+};
