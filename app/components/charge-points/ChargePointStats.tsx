5600cc6cc2e09434dc33497ecd742a95df0587ee
Adrien MIQUEL <adrien.miquel@spendesk.com>
2026-03-21 23:17:15 +0100

    feat(dashboard): Revamp grid stat card

--- app/components/charge-points/ChargePointStats.tsx
+++ app/components/charge-points/ChargePointStats.tsx
@@ -1,6 +1,8 @@
-import { ChargePoint } from "@/types/charge-point";
+import { ReactNode } from "react";
+import { Battery, CheckCircle, Cloud, RefreshCw, X } from "lucide-react";
+
+import { ChargePoint, ChargePointStatus } from "@/types/charge-point";
 import { StatCard } from "../common/StatCard";
-import { Battery, PlugZap, RefreshCw, X } from "lucide-react";
 
 export const ChargePointStats = ({
   chargePoints,
@@ -9,61 +11,123 @@ export const ChargePointStats = ({
 }) => {
   const stats = {
     total: chargePoints.length,
-    connected: chargePoints.filter((cp) => cp.connection.status === "CONNECTED")
-      .length,
-    synced: chargePoints.filter((cp) => cp.connection.status === "SYNCED")
-      .length,
     offline: chargePoints.filter((cp) => cp.connection.status === "OFFLINE")
       .length,
-    // available: chargePoints.filter(
-    //   (cp) => cp.status === "Available" && cp.lifecycle === "SYNCED",
-    // ).length,
-    // charging: chargePoints.filter((cp) => cp.status === "Charging").length,
-    // faulted: chargePoints.filter(
-    //   (cp) => cp.status === "Faulted" || cp.lifecycle === "OFFLINE",
-    // ).length,
+    errorOrFaulted: chargePoints.filter(
+      (cp) => cp.connection.status === "ERROR" || cp.status === "Faulted",
+    ).length,
+    available: chargePoints.filter(
+      (cp) => cp.status === "Available" && cp.connection.status === "SYNCED",
+    ).length,
+    charging: chargePoints.filter((cp) => cp.status === "Charging").length,
+    faulted: chargePoints.filter(
+      (cp) => cp.status === "Faulted" || cp.connection.status === "OFFLINE",
+    ).length,
   };
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-4 gap-4 mb-8">
-      <StatCard
-        title="Total"
-        value={stats.total}
-        icon={<Battery className="h-5 w-5 text-gray-600" />}
-      />
-      <StatCard
-        title="Connected"
-        value={stats.connected}
-        icon={<PlugZap className="h-5 w-5 text-yellow-600" />}
-        subtitle={`${stats.total > 0 ? Math.round((stats.connected / stats.total) * 100) : 0}%`}
-      />
-      <StatCard
-        title="Synced"
-        value={stats.synced}
-        icon={<RefreshCw className="h-5 w-5 text-green-600" />}
-        subtitle={`${stats.total > 0 ? Math.round((stats.synced / stats.total) * 100) : 0}%`}
-      />
-      <StatCard
-        title="Offline"
-        value={stats.offline}
-        icon={<X className="h-5 w-5 text-red-600" />}
-        subtitle={`${stats.total > 0 ? Math.round((stats.offline / stats.total) * 100) : 0}%`}
-      />
-      {/* <StatCard
-        title="Available"
-        value={stats.available}
-        icon={<CheckCircle className="h-5 w-5 text-green-600" />}
-      />
-      <StatCard
-        title="Charging"
-        value={stats.charging}
-        icon={<Loader className="h-5 w-5 text-blue-600" />}
-      />
-      <StatCard
-        title="Faulted"
-        value={stats.faulted}
-        icon={<AlertCircle className="h-5 w-5 text-red-600" />}
-      /> */}
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:col-span-2">
+        <StatCard
+          title="Total"
+          value={stats.total}
+          icon={<Battery className="h-5 w-5 text-gray-600" />}
+        />
+        <StatCard
+          title="Available"
+          value={stats.available}
+          icon={<CheckCircle className="h-5 w-5 text-green-600" />}
+        />
+        <StatCard
+          title="Offline"
+          value={stats.offline}
+          icon={<RefreshCw className="h-5 w-5 text-green-600" />}
+          subtitle={`${stats.total > 0 ? Math.round((stats.offline / stats.total) * 100) : 0}%`}
+        />
+        <StatCard
+          title="Offline / Error / Faulted"
+          value={stats.errorOrFaulted}
+          icon={<X className="h-5 w-5 text-red-600" />}
+          subtitle={`${stats.total > 0 ? Math.round((stats.errorOrFaulted / stats.total) * 100) : 0}%`}
+        />
+      </div>
+      <div className="h-full md:col-span-1 [&>div]:h-full">
+        <StatCard
+          title="Synced"
+          value={stats.charging}
+          icon={<Cloud className="h-5 w-5 text-blue-600" />}
+        >
+          <div className="flex flex-col content-stretch gap-2 text-sm mt-2">
+            {(
+              [
+                "Available",
+                "Preparing",
+                "Charging",
+                "Finishing",
+                "Reserved",
+                "Unavailable",
+              ] as ChargePointStatus[]
+            ).map((status) => (
+              <div
+                key={status.toLowerCase()}
+                className="flex items-center gap-2 justify-between"
+              >
+                <div className="flex items-center gap-2">
+                  {getStatusIcon(status)}
+                  <p className="text-md italic">{status}</p>
+                </div>
+                <div className="flex items-center gap-2">
+                  <p className="ml-auto text-sm font-bold text-gray-900">
+                    {
+                      chargePoints.filter(
+                        (cp) =>
+                          cp.status === status &&
+                          cp.connection.status === "SYNCED",
+                      ).length
+                    }
+                  </p>
+                  <p className="text-sm text-gray-500">
+                    (
+                    {stats.total > 0
+                      ? Math.round(
+                          (chargePoints.filter(
+                            (cp) =>
+                              cp.status === status &&
+                              cp.connection.status === "SYNCED",
+                          ).length /
+                            stats.total) *
+                            100,
+                        )
+                      : 0}
+                    %)
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </StatCard>
+      </div>
     </div>
   );
+};
+
+const getStatusIcon = (status: ChargePointStatus): ReactNode => {
+  switch (status) {
+    case "Available":
+      return <CheckCircle className="h-5 w-5 text-green-600" />;
+    case "Preparing":
+    case "Finishing":
+      return <RefreshCw className="h-5 w-5 text-yellow-600" />;
+    case "Charging":
+      return <Battery className="h-5 w-5 text-blue-600" />;
+    case "SuspendedEV":
+    case "SuspendedEVSE":
+    case "Reserved":
+    case "Unavailable":
+      return <Cloud className="h-5 w-5 text-orange-700" />;
+    case "Faulted":
+      return <X className="h-5 w-5 text-red-600" />;
+    default:
+      return null;
+  }
 };
--- app/components/common/StatCard.tsx
+++ app/components/common/StatCard.tsx
@@ -1,19 +1,28 @@
-interface StatCardProps {
+import { PropsWithChildren } from "react";
+
+type StatCardProps = {
   title: string;
   value: number;
   icon: React.ReactNode;
   subtitle?: string;
-}
+} & PropsWithChildren;
 
-export const StatCard = ({ title, value, icon, subtitle }: StatCardProps) => {
+export const StatCard = ({
+  title,
+  value,
+  icon,
+  subtitle,
+  children,
+}: StatCardProps) => {
   return (
-    <div className="bg-white rounded-lg border border-gray-200 p-4">
+    <div className="bg-white rounded-lg border border-gray-200 p-4 flex flex-col content-stretch">
       <div className="flex items-center justify-between mb-2">
         <p className="text-sm font-medium text-gray-600">{title}</p>
         {icon}
       </div>
       <p className="text-2xl font-bold text-gray-900">{value}</p>
       {subtitle && <p className="text-xs text-gray-500 mt-1">{subtitle}</p>}
+      {children}
     </div>
   );
 };
