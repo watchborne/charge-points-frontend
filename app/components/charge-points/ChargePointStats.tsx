29da72bd3ec9583261994438e314828900801f58
Adrien MIQUEL <adrien.miquel@spendesk.com>
2026-03-23 20:47:52 +0100

    refacto(cp): Refacto CP stats

--- app/components/charge-points/ChargePointStats.tsx
+++ app/components/charge-points/ChargePointStats.tsx
@@ -1,5 +1,12 @@
 import { ReactNode } from "react";
-import { Battery, CheckCircle, Cloud, RefreshCw, X } from "lucide-react";
+import {
+  Battery,
+  CheckCircle,
+  Cloud,
+  PlugZap,
+  RefreshCw,
+  X,
+} from "lucide-react";
 
 import { ChargePoint, ChargePointStatus } from "@/types/charge-point";
 import { StatCard } from "../common/StatCard";
@@ -9,18 +16,35 @@ export const ChargePointStats = ({
 }: {
   chargePoints: ChargePoint[];
 }) => {
+  const connectedDevices = chargePoints.filter(({ connection }) =>
+    ["SYNCED", "CONNECTED"].includes(connection.status),
+  );
+  const offlineDevices = chargePoints.filter(({ connection }) =>
+    ["OFFLINE"].includes(connection.status),
+  );
+  const notStableDevices = chargePoints.filter(
+    (cp) => cp.connection.status === "WARNING" || cp.status === "Faulted",
+  );
+  const syncedDevices = chargePoints.filter(
+    (cp) => cp.connection.status === "SYNCED",
+  );
+
+  const makePercentage = (count: number) =>
+    chargePoints.length > 0
+      ? `${Math.round((count / chargePoints.length) * 100)}%`
+      : "0%";
+
+  const makeStat = (count: number) => ({
+    value: count,
+    percentage: makePercentage(count),
+  });
+
   const stats = {
     total: chargePoints.length,
-    available: chargePoints.filter(
-      (cp) => cp.status === "Available" && cp.connection.status === "SYNCED",
-    ).length,
-    offline: chargePoints.filter((cp) => cp.connection.status === "OFFLINE")
-      .length,
-    notStable: chargePoints.filter(
-      (cp) => cp.connection.status === "WARNING" || cp.status === "Faulted",
-    ).length,
-    synced: chargePoints.filter((cp) => cp.connection.status === "SYNCED")
-      .length,
+    connected: makeStat(connectedDevices.length),
+    offline: makeStat(offlineDevices.length),
+    notStable: makeStat(notStableDevices.length),
+    synced: makeStat(syncedDevices.length),
   };
 
   return (
@@ -32,27 +56,28 @@ export const ChargePointStats = ({
           icon={<Battery className="h-5 w-5 text-gray-600" />}
         />
         <StatCard
-          title="Available"
-          value={stats.available}
-          icon={<CheckCircle className="h-5 w-5 text-green-600" />}
+          title="Connected"
+          value={stats.connected.value}
+          icon={<PlugZap className="h-5 w-5 text-yellow-600" />}
+          subtitle={stats.connected.percentage}
         />
         <StatCard
           title="Offline"
-          value={stats.offline}
+          value={stats.offline.value}
           icon={<RefreshCw className="h-5 w-5 text-green-600" />}
-          subtitle={`${stats.total > 0 ? Math.round((stats.offline / stats.total) * 100) : 0}%`}
+          subtitle={stats.offline.percentage}
         />
         <StatCard
           title="Not stable"
-          value={stats.notStable}
+          value={stats.notStable.value}
           icon={<X className="h-5 w-5 text-red-600" />}
-          subtitle={`${stats.total > 0 ? Math.round((stats.notStable / stats.total) * 100) : 0}%`}
+          subtitle={stats.notStable.percentage}
         />
       </div>
       <div className="h-full md:col-span-1 [&>div]:h-full">
         <StatCard
           title="Synced"
-          value={stats.synced}
+          value={stats.synced.value}
           icon={<Cloud className="h-6 w-6 text-green-600" />}
         >
           <div className="flex flex-col content-stretch gap-2 text-sm mt-2">
@@ -65,43 +90,32 @@ export const ChargePointStats = ({
                 "Reserved",
                 "Unavailable",
               ] as ChargePointStatus[]
-            ).map((status) => (
-              <div
-                key={status.toLowerCase()}
-                className="flex items-center gap-2 justify-between"
-              >
-                <div className="flex items-center gap-2">
-                  {getStatusIcon(status)}
-                  <p className="text-md italic">{status}</p>
+            ).map((status) => {
+              const countForStatus = chargePoints.filter(
+                (cp) =>
+                  cp.status === status && cp.connection.status === "SYNCED",
+              ).length;
+
+              return (
+                <div
+                  key={status.toLowerCase()}
+                  className="flex items-center gap-2 justify-between"
+                >
+                  <div className="flex items-center gap-2">
+                    {getStatusIcon(status)}
+                    <p className="text-md italic">{status}</p>
+                  </div>
+                  <div className="flex items-center gap-2">
+                    <p className="ml-auto text-sm font-bold text-gray-900">
+                      {countForStatus}
+                    </p>
+                    <p className="text-sm text-gray-500">
+                      ({makePercentage(countForStatus)})
+                    </p>
+                  </div>
                 </div>
-                <div className="flex items-center gap-2">
-                  <p className="ml-auto text-sm font-bold text-gray-900">
-                    {
-                      chargePoints.filter(
-                        (cp) =>
-                          cp.status === status &&
-                          cp.connection.status === "SYNCED",
-                      ).length
-                    }
-                  </p>
-                  <p className="text-sm text-gray-500">
-                    (
-                    {stats.total > 0
-                      ? Math.round(
-                          (chargePoints.filter(
-                            (cp) =>
-                              cp.status === status &&
-                              cp.connection.status === "SYNCED",
-                          ).length /
-                            stats.total) *
-                            100,
-                        )
-                      : 0}
-                    %)
-                  </p>
-                </div>
-              </div>
-            ))}
+              );
+            })}
           </div>
         </StatCard>
       </div>
