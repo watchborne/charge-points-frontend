fe03e05a79883eab735817dc4792b828f014580b
Adrien Miquel <47077258+AvocadoVenom@users.noreply.github.com>
2026-06-18 23:23:55 +0200

    chore(i18n): Add next-intl library and base configuration (#24)
    Co-authored-by: AvocadoVenom <--remove>

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
+const createNextIntlPlugin = require("next-intl/plugin");
+
+const withNextIntl = createNextIntlPlugin("./i18n/request.ts");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
     missingSuspenseWithCSRBailout: false,
   },
+};
 
-}
-
-module.exports = nextConfig
+module.exports = withNextIntl(nextConfig);
