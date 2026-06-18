6c81402f4ee8afabc884175d758182d91e68d4e4
Adrien Miquel <47077258+AvocadoVenom@users.noreply.github.com>
2026-06-18 12:55:47 +0200

    Add translation key into marketing homepage

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,10 +1,14 @@
+import { useTranslations } from "next-intl";
+
 export default function MarketingHomePage() {
+  const t = useTranslations();
+
   return (
     <div className="min-h-screen bg-gray-50">
       <h1>Marketing website</h1>
 
       <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-        Homepage
+        {t("appName")} Homepage
       </main>
     </div>
   );
