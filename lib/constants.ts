104d88835a1a7fe9b70ee39cdbcb175dd237efdd
QiMirda <admiquel94@gmail.com>
2026-06-25 14:26:15 +0200

    Fix format

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,5 +1,3 @@
-export const API_URL =
-  process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
 
-export const WS_URL =
-  process.env.NEXT_PUBLIC_WS_URL || "ws://localhost:3000/ws";
+export const WS_URL = process.env.NEXT_PUBLIC_WS_URL || "ws://localhost:3000/ws";
