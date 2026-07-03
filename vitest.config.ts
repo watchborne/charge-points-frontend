855008a2e9511c263484930b8f1009ae49e8425a
Adrien Miquel <47077258+AvocadoVenom@users.noreply.github.com>
2026-07-03 11:47:32 +0200

    ci(tests): Switch to GitHub Actions Vitest reporter (#58)

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,18 @@ export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
     environment: "jsdom",
+    reporters:
+      process.env.GITHUB_ACTIONS === "true"
+        ? [
+            "tree",
+            [
+              "github-actions",
+              {
+                onWritePath: (path) => path.replace(/^\/app\//, `${process.env.GITHUB_WORKSPACE}/`),
+              },
+            ],
+          ]
+        : ["tree"],
     exclude: ["**/node_modules/**", "**/dist/**"],
   },
 });
