27490efacc3571ae6be16e4361fd5bd950a8c617
QiMirda <admiquel94@gmail.com>
2026-07-19 12:52:56 +0200

    Remove comment

--- app/components/ToastNotification/ToastNotification.tsx
+++ app/components/ToastNotification/ToastNotification.tsx
@@ -2,12 +2,6 @@
 
 import { Toaster, type ToasterProps } from "sonner";
 
-/**
- * Point de rendu global des notifications.
- *
- * Ce composant encapsule volontairement Sonner afin que le reste
- * de l'application ne dépende pas directement de la librairie.
- */
 export function ToastNotification(props: ToasterProps) {
   return <Toaster position="top-right" richColors {...props} />;
 }
