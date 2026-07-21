a097de22ce5f80615ece3535052c386d873521c1
Adrien Miquel <47077258+AvocadoVenom@users.noreply.github.com>
2026-07-21 23:25:47 +0200

    feat(components): Toast - Change toast position (#147)

--- app/components/ToastNotification/ToastNotification.tsx
+++ app/components/ToastNotification/ToastNotification.tsx
@@ -7,7 +7,7 @@ const DEFAULT_DURATION_MS = 15_000;
 export function ToastNotification(props: ToasterProps) {
   return (
     <Toaster
-      position="bottom-right"
+      position="bottom-center"
       richColors
       closeButton
       duration={DEFAULT_DURATION_MS}
