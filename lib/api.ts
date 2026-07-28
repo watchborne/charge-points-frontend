8c670d72a2596fa88330543e67ae2a80f429c61d
Adrien Miquel <47077258+AvocadoVenom@users.noreply.github.com>
2026-07-28 23:55:59 +0200

    feat(api): add GET /api/me proxy and client method (#162)

--- lib/api.ts
+++ lib/api.ts
@@ -1,9 +1,11 @@
 import { accessRequestApis } from "./api-access-requests";
 import { chargePointApis } from "./api-charge-points";
+import { meApis } from "./api-me";
 import { siteApis } from "./api-sites";
 
 export const api = {
   AccessRequests: accessRequestApis,
   ChargePoints: chargePointApis,
   Sites: siteApis,
+  Me: meApis,
 };
