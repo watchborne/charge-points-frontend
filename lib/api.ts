3d599973ba38cf87d136fc9b10e78584d3ad2f89
Adrien Miquel <47077258+AvocadoVenom@users.noreply.github.com>
2026-08-20 22:18:02 +0200

    feat(charge-points): show a charge point's security-event history (#295)

--- lib/api.ts
+++ lib/api.ts
@@ -3,6 +3,7 @@ import { chargePointApis } from "./api-charge-points";
 import { commissioningTokenApis } from "./api-commissioning-token";
 import { meApis } from "./api-me";
 import { meteringApis } from "./api-metering";
+import { securityEventApis } from "./api-security-events";
 import { siteApis } from "./api-sites";
 import { statusHistoryApis } from "./api-status-history";
 
@@ -14,4 +15,5 @@ export const api = {
   CommissioningToken: commissioningTokenApis,
   Metering: meteringApis,
   StatusHistory: statusHistoryApis,
+  SecurityEvents: securityEventApis,
 };
