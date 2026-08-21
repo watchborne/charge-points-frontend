16f8029f403d8e10ebf783517621b9486c2dc385
Adrien Miquel <47077258+AvocadoVenom@users.noreply.github.com>
2026-08-22 00:23:24 +0200

    fix(layout):Fix responsiveness (#302)

--- app/[locale]/app/components/sites/SiteStats.tsx
+++ app/[locale]/app/components/sites/SiteStats.tsx
@@ -22,7 +22,7 @@ export const SiteStats = ({
     chargePoints.length > 0 ? `${Math.round((count / chargePoints.length) * 100)}%` : "0%";
 
   return (
-    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-4">
+    <div className="grid gap-4 grid-cols-2 md:grid-cols-4">
       <StatCard
         title={t("appPage.sites.page.stats.totalSites")}
         value={sites.length}
